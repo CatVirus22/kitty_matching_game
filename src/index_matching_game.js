c5868349fc7977d5671606d77bc9cc96f2384dc5
Marizabelle Aldana <94816917+CatVirus22@users.noreply.github.com>
2022-10-17 18:16:18 -0600

    updated function click, addd color

--- src/index_matching_game.js
+++ src/index_matching_game.js
@@ -1,7 +1,33 @@
-function disableBtn(btn) {
-  btn.disabled = true;
-}
+let checkbox1 = document.querySelector("#box1");
+let checkbox2 = document.querySelector("#box2");
+let checkbox3 = document.querySelector("#box3");
 
+checkbox1.onclick = function () {
+  if (checkbox1.checked === true) {
+    checkbox1.disabled = "true";
+  }
+};
+
+checkbox2.onclick = function () {
+  if (checkbox2.checked === true) {
+    checkbox2.disabled = "true";
+  }
+};
+
+checkbox3.onclick = function () {
+  if (checkbox3.checked === true) {
+    checkbox3.disabled = "true";
+  }
+};
 function resetBox() {
-  document.getElementById("checkbox").reset();
+  document.querySelector("#checkboxes").reset();
+
+  checkbox1.disabled = false;
+  checkbox1.checked = false;
+
+  checkbox2.disabled = false;
+  checkbox2.checked = false;
+
+  checkbox3.disabled = false;
+  checkbox3.checked = false;
 }
